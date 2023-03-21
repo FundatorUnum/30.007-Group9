839c386a34c5f71f24dd7d0444681dafb2dd3c53
Mathurah Ravigulan <mathurah25@gmail.com>
2023-03-21 18:48:01 +0800

    Added mk5

--- TransmitterTestMk5/TransmitterTestMk5.ino
+++ TransmitterTestMk5/TransmitterTestMk5.ino
@@ -1,8 +1,10 @@
 /*
 Created by Yvan / https://Brainy-Bits.com
+
 This code is in the public domain...
 You can: copy it, use it, modify it, share it or just plain ignore it!
 Thx!
+
 */
 
 // NRF24L01 Module Tutorial - Code for Transmitter using Arduino NANO
@@ -12,13 +14,16 @@ Thx!
 #include "RF24.h"
 #include "SPI.h"
 
-#define SwitchPin1 10  // Arcade switch is connected to Pin 10 on NANO
-#define SwitchPin2 6
-#define SwitchPin3 5
-#define SwitchPin4 4
-#define SwitchPin5 3
+int fsrPin = 0;  // the FSR and 10K pulldown are connected to a0
+int fsrReading;  // the analog reading from the FSR resistor divider
 
-int SentMessage[5] = { 0, 0, 0, 0, 0 };  // Used to store value before being sent through the NRF24L01
+#define SwitchPin1 A0  // Arcade switch is connected to Pin 10 on NANO
+#define SwitchPin2 A1
+#define SwitchPin3 A2
+#define SwitchPin4 A3
+#define SwitchPin5 A4
+
+int SentMessage[5] = { fsr0, 0, 0, 0, 0 };  // Used to store value before being sent through the NRF24L01
 
 RF24 radio(7, 8);  // NRF24L01 used SPI pins + Pin 9 and 10 on the NANO
 
@@ -27,42 +32,31 @@ const uint64_t pipe = 0xE6E6E6E6E6E6;  // Needs to be the same for communicating
 
 void setup() {
   Serial.begin(9600);
-  pinMode(SwitchPin1, INPUT_PULLUP);  // Define the arcade switch NANO pin as an Input using Internal Pullups
-  digitalWrite(SwitchPin1, HIGH);     // Set Pin to HIGH at beginning
-  pinMode(SwitchPin2, INPUT_PULLUP);
-  digitalWrite(SwitchPin2, HIGH);
-  pinMode(SwitchPin3, INPUT_PULLUP);
-  digitalWrite(SwitchPin3, HIGH);
-  pinMode(SwitchPin4, INPUT_PULLUP);
-  digitalWrite(SwitchPin4, HIGH);
-  pinMode(SwitchPin5, INPUT_PULLUP);
-  digitalWrite(SwitchPin5, HIGH);
-
   radio.begin();                // Start the NRF24L01
   radio.openWritingPipe(pipe);  // Get NRF24L01 ready to transmit
 }
 
 void loop(void) {
 
-  if (digitalRead(SwitchPin1) == LOW) {  // If Switch is Activated
+  if (analogRead(SwitchPin1) == LOW) {  // If Switch is Activated
     SentMessage[0] = 1;
     Serial.write("Button 1 Pressed\n");
   } else {
     SentMessage[0] = 0;
   }
-  if (digitalRead(SwitchPin2) == LOW) {
+  if (analogRead(SwitchPin2) == LOW) {
     SentMessage[1] = 1;
     Serial.write("Button 2 Pressed\n");
   } else {
     SentMessage[1] = 0;
   }
-  if (digitalRead(SwitchPin3) == LOW) {
+  if (analogRead(SwitchPin3) == LOW) {
     SentMessage[2] = 1;
     Serial.write("Button 3 Pressed\n");
   } else {
     SentMessage[2] = 0;
   }
-  if (digitalRead(SwitchPin4) == LOW) {
+  if (analogRead(SwitchPin4) == LOW) {
     SentMessage[3] = 1;
     Serial.write("Button 4 Pressed\n");
   } else {
@@ -74,6 +68,23 @@ void loop(void) {
   } else {
     SentMessage[4] = 0;
   }
+  fsr0 = analogRead(fsrPin);
+
+  Serial.print("Analog reading = ");
+  Serial.print(fsrReading);  // print the raw analog reading
+
+  if (fsrReading < 10) {
+    Serial.println(" - No pressure");
+  } else if (fsrReading < 200) {
+    Serial.println(" - Light touch");
+  } else if (fsrReading < 500) {
+    Serial.println(" - Light squeeze");
+  } else if (fsrReading < 800) {
+    Serial.println(" - Medium squeeze");
+  } else {
+    Serial.println(" - Big squeeze");
+  }
+  delay(1000);
 
   radio.write(SentMessage, sizeof(SentMessage));  // Send value through NRF24L01
 }
