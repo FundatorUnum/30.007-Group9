173c7ac0f9eec86435b1fe62430af22bc6633f55
Gareth Tan <gartan12@gmail.com>
2023-03-29 22:51:19 +0800

    New code for two transmitter, one receiver

--- Receiver_TestMK6/Receiver_TestMK6.ino
+++ Receiver_TestMK6/Receiver_TestMK6.ino
@@ -21,18 +21,20 @@ Thx!
 //#define LED_PIN4 (number) (not needed because we're using serial monitor to test)
 //#define LED_PIN5 (number) (not needed because we're using serial monitor to test)
 
-int ReceivedMessage[5] = {0,0,0,0,0}; // Used to store value received by the NRF24L01
+int ReceivedMessage[6] = {0,0,0,0,0,0}; // Used to store value received by the NRF24L01
+int ReceivedMessageL[6] = {0,0,0,0,0,0}; // Store value receieved from the other NRF24L01
 
 RF24 radio(7,8); // NRF24L01 used SPI pins + Pin 7 and 8 on the UNO
 
-const uint64_t pipe = 0xE6E6E6E6E6E6; // Needs to be the same for communicating between 2 NRF24L01 
-
+const uint64_t pipe1 = 0xE6E6E6E6E6E6; // Needs to be the same for communicating between 2 NRF24L01 
+const uint64_t pipe2 = 0xA6A6A6A6A6A6;
 
 void setup(void){
 Serial.begin(9600);
 radio.begin(); // Start the NRF24L01
 
-radio.openReadingPipe(0,pipe); // Get NRF24L01 ready to receive
+radio.openReadingPipe(0,pipe1); // Get NRF24L01 ready to receive
+radio.openReadingPipe(1,pipe2);
 
 radio.startListening(); // Listen to see if information received
 
@@ -51,12 +53,14 @@ radio.startListening(); // Listen to see if information received
 }
 void loop(void){
 
-while (radio.available())
+while (radio.available(0))
 {
 
 radio.read(ReceivedMessage, sizeof(ReceivedMessage)); // Read information from the NRF24L01
+radio.read(ReceivedMessageL, sizeof(ReceivedMessageL));
 
-  Serial.print("Analog reading (Thumb) = ");
+if (ReceivedMessage[5] == 0){
+  Serial.print("Analog reading (R_Thumb) = ");
   Serial.print(ReceivedMessage[0]);  // print the raw analog reading
 if (ReceivedMessage[0] < 10) {
     Serial.println(" - No pressure");
@@ -70,7 +74,7 @@ if (ReceivedMessage[0] < 10) {
     Serial.println(" - Big squeeze");
   }
 
-  Serial.print("Analog reading (Index) = ");
+  Serial.print("Analog reading (R_Index) = ");
   Serial.print(ReceivedMessage[1]);  // print the raw analog reading
 if (ReceivedMessage[1] < 10) {
     Serial.println(" - No pressure");
@@ -84,19 +88,50 @@ if (ReceivedMessage[1] < 10) {
     Serial.println(" - Big squeeze");
   }
 
-  Serial.print("Analog reading (Middle) = ");
+  Serial.print("Analog reading (R_Middle) = ");
   Serial.print(ReceivedMessage[2]);  // print the raw analog reading
 if (ReceivedMessage[2] < 10) {
-    Serial.println(" - No pressure\n");
+    Serial.println(" - No pressure");
   } else if (ReceivedMessage[2] < 200) {
-    Serial.println(" - Light touch\n");
+    Serial.println(" - Light touch");
   } else if (ReceivedMessage[2] < 500) {
-    Serial.println(" - Light squeeze\n");
+    Serial.println(" - Light squeeze");
   } else if (ReceivedMessage[2] < 800) {
+    Serial.println(" - Medium squeeze");
+  } else {
+    Serial.println(" - Big squeeze");
+  }
+}
+
+if (ReceivedMessage[5] == 1){
+  Serial.println("//////////////////");
+  Serial.print("Analog reading (L_Thumb) = ");
+  Serial.print(ReceivedMessage[0]);  // print the raw analog reading
+if (ReceivedMessage[0] < 10) {
+    Serial.println(" - No pressure");
+  } else if (ReceivedMessage[0] < 200) {
+    Serial.println(" - Light touch");
+  } else if (ReceivedMessage[0] < 500) {
+    Serial.println(" - Light squeeze");
+  } else if (ReceivedMessage[0] < 800) {
+    Serial.println(" - Medium squeeze");
+  } else {
+    Serial.println(" - Big squeeze");
+  }
+  Serial.print("Analog reading (L_Index) = ");
+  Serial.print(ReceivedMessage[1]);  // print the raw analog reading
+if (ReceivedMessage[1] < 10) {
+    Serial.println(" - No pressure\n");
+  } else if (ReceivedMessage[1] < 200) {
+    Serial.println(" - Light touch\n");
+  } else if (ReceivedMessage[1] < 500) {
+    Serial.println(" - Light squeeze\n");
+  } else if (ReceivedMessage[1] < 800) {
     Serial.println(" - Medium squeeze\n");
   } else {
     Serial.println(" - Big squeeze\n");
   }
-  delay(100);
+delay(10);
+}
 }
 }
---
+++ RightHandMK1/RightHandMK1.ino
@@ -0,0 +1,88 @@
+//Created by Yvan / https://Brainy-Bits.com
+//This code is in the public domain...
+//You can: copy it, use it, modify it, share it or just plain ignore it!
+//Thx!
+// NRF24L01 Module Tutorial - Code for Transmitter using Arduino NANO
+
+//Include needed Libraries at beginning
+#include "nRF24L01.h"  //NRF24L01 library created by TMRh20 https://github.com/TMRh20/RF24
+#include "RF24.h"
+#include "SPI.h"
+
+int FsrPin1 = A0;  // the FSR and 10K pulldown are connected to a0
+int fsrReading1;  // the analog reading from the FSR resistor divider
+
+int FsrPin2 = A1;
+int fsrReading2;
+
+int FsrPin3 = A2;
+int fsrReading3;
+
+//int FsrPin4 = A3;
+//int fsrReading4;
+//
+//int FsrPin5 = A4;
+//int fsrReading5;
+
+
+
+int SentMessage[6] = { 0, 0, 0, 0, 0, 0 };  // Used to store value before being sent through the NRF24L01
+
+RF24 radio(7, 8);  // NRF24L01 used SPI pins + Pin 9 and 10 on the NANO
+
+const uint64_t pipe = 0xE6E6E6E6E6E6;  // Needs to be the same for communicating between 2 NRF24L01
+
+//int Array[1];
+
+void setup() {
+  Serial.begin(9600);           // establish serial communication
+  radio.begin();                // Start the NRF24L01
+  radio.openWritingPipe(pipe);  // Get NRF24L01 ready to transmit
+}
+
+void loop(void) {
+
+
+//  fsrReading1 = analogRead(FsrPin1);
+//  Serial.print("Analog reading = ");
+//  Serial.print(fsrReading1);  // print the raw analog reading
+//
+//  fsrReading2 = analogRead(FsrPin2);
+//  Serial.print("Analog reading = ");
+//  Serial.print(fsrReading2);  // print the raw analog reading
+//
+//  fsrReading3 = analogRead(FsrPin3);
+//  Serial.print("Analog reading = ");
+//  Serial.print(fsrReading3);  // print the raw analog reading
+
+//  fsrReading4 = analogRead(FsrPin4);
+//  Serial.print("Analog reading = ");
+//  Serial.print(fsrReading4);  // print the raw analog reading
+//
+//  fsrReading5 = analogRead(FsrPin5);
+//  Serial.print("Analog reading = ");
+//  Serial.print(fsrReading5);  // print the raw analog reading
+
+  SentMessage[0] = analogRead(FsrPin1);
+  SentMessage[1] = analogRead(FsrPin2);
+  SentMessage[2] = analogRead(FsrPin3);
+//  SentMessage[3] = analogRead(FsrPin4);
+//  SentMessage[4] = analogRead(FsrPin5);
+
+//  if (SentMessage[0] < 10) {
+//    Serial.println(" - No pressure");
+//  } else if (SentMessage[0] < 200) {
+//    Serial.println(" - Light touch");
+//  } else if (SentMessage[0] < 500) {
+//    Serial.println(" - Light squeeze");
+//  } else if (SentMessage[0] < 800) {
+//    Serial.println(" - Medium squeeze");
+//  } else {
+//    Serial.println(" - Big squeeze");
+//  }
+//  delay(1000);
+  
+  radio.write(SentMessage, sizeof(SentMessage));  // Send value through NRF24L01
+  Serial.println("Sending");
+  delay(20);
+}
